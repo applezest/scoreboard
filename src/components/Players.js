decf8c5021e94d78f1bf47bde1971f75fcfd8f1c
kosmo <soomi@google.co.kr>
2019-08-31 13:20:22 +0900

    PureComponent로 성능 개선 (증가 혹은 감소만 일어난 Player만 리렌더링)

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import { Counter } from './Counter';
 
+export class Player extends React.PureComponent{
+	render(){
+		console.log(this.props.name, 'rendered')
+
+		return (
+			<div className='player'>
+			<span className='player-name'>
+				<button className='remove-player' onClick={() => this.props.removePlayer(this.props.id)}>x</button>
+				{this.props.name}
+			</span>
+				<Counter id={this.props.id} score={this.props.score} changeScore={this.props.changeScore}/>
+			</div>
+		)
+	}
+}
+/*
 export const Player = (props) => (
 	<div className='player'>
     <span className='player-name'>
@@ -10,3 +26,4 @@ export const Player = (props) => (
 		<Counter id={props.id} score={props.score} changeScore={props.changeScore}/>
 	</div>
 );
+*/
